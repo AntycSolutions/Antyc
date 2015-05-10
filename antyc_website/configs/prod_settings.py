1e9e96a6949d6dc6a56c4ba4259140a06ee3333d
Solanar <and_555@hotmail.com>
2015-05-09 23:13:33 -0600

    additional security settings, cache templates

--- antyc_website/configs/prod_settings.py
+++ antyc_website/configs/prod_settings.py
@@ -35,9 +35,23 @@ EMAIL_HOST_PASSWORD = ''
 
 EMAIL_SUBJECT_PREFIX = '[Antyc Solutions]'
 
+# Cache templates
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader',
+        ('django.template.loaders.filesystem.Loader',
+         'django.template.loaders.app_directories.Loader',)
+     ),
+)
+
 # HTTPS/SSL
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 os.environ['wsgi.url_scheme'] = 'https'
+CSRF_COOKIE_HTTPONLY = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_SSL_REDIRECT = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_SECONDS = 15768000
