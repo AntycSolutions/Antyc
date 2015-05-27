1fd93485d4d91ceb457c1f819d8cc6bf84687c61
Solanar <and_555@hotmail.com>
2015-05-27 15:31:57 -0600

    add requirecss, move css to requirecss

--- assets/js/require_setup.js
+++ assets/js/require_setup.js
@@ -1,15 +1,31 @@
 requirejs.config({
-    paths: {
-        jquery: "//code.jquery.com/jquery-2.1.3.min",
-        bootstrap: "//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min",
+    "paths": {
+        // jQuery
+        "jquery": "//code.jquery.com/jquery-2.1.3.min",
+        // Bootstrap
+        "bootstrap_js": "//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min",
+        // Bootswatch Simplex Bootstrap
+        "bootstrap_css": "//maxcdn.bootstrapcdn.com/bootswatch/3.3.2/simplex/bootstrap.min",
+        // Font Awesome
+        "font_awesome": "//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min"
     },
-    shim: {
-        bootstrap: {
-            deps: ["jquery"]
+    "shim": {
+        "bootstrap_js": {
+            "deps": ["jquery"]
+        }
+    },
+    "map": {
+        '*': {
+            // require-css
+            'css': '//cdnjs.cloudflare.com/ajax/libs/require-css/0.1.8/css.min.js'
         }
     }
 });
 
-requirejs(["jquery", "bootstrap"], function ($) {
-
-});
+require(
+    ["css!bootstrap_css", "css!font_awesome",
+     "jquery", "bootstrap_js"],
+    function ($) {
+        // console.log("works");
+    }
+);
