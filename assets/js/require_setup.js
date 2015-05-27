18301db4734888ed27bcb030689921d1e7024816
Solanar <and_555@hotmail.com>
2015-05-26 19:11:31 -0600

    combine 2 require js files into one

--- assets/js/require_setup.js
+++ assets/js/require_setup.js
@@ -2,7 +2,6 @@ requirejs.config({
     paths: {
         jquery: "//code.jquery.com/jquery-2.1.3.min",
         bootstrap: "//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min",
-        antyc: "antyc"
     },
     shim: {
         bootstrap: {
@@ -11,4 +10,6 @@ requirejs.config({
     }
 });
 
-requirejs(["antyc"]);
+requirejs(["jquery", "bootstrap"], function ($) {
+
+});
