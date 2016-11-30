fec50312d8b0b0d0f43db10ae22e22527291ccec
Solanar <and_555@hotmail.com>
2016-11-30 14:33:37 -0700

    fix typo

--- activate.sh
+++ activate.sh
@@ -2,9 +2,9 @@
 platform=`python -c "import platform; print(platform.system())"`
 
 if [[ "$platform" == 'Linux' ]]; then
-    source ../venv_perfect_arch/bin/activate
+    source ../venv_antyc_website/bin/activate
 elif [[ "$platform" == 'Windows' ]]; then
-    source ../venv_perfect_arch/Scripts/activate
+    source ../venv_antyc_website/Scripts/activate
 else
     echo "Unsupported platform: $platform"
 fi
